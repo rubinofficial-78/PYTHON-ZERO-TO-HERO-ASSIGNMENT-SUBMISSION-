bc015c6744f9fd7ed90ffe0da4cbd64867a1e1c8
rubinofficial-78 <91709659+rubinofficial-78@users.noreply.github.com>
2021-10-08 07:28:06 +0530

    Day 4 Assignment Submitted : )

---
+++ DAY - 4 LetsUpgrade Python.py
@@ -0,0 +1,5 @@
+#Python LetsUpgrade Assignment
+def employee_details(emp_id,emp_name,contact,address,salary):
+    print(emp_id,'is the Employee Id of Mr/Ms',emp_name,'whose contact number is',contact,'whose address is',address,'his/her salary is',salary)
+employee_details('20CS107','Rubin',9344864492,'Coimbatore',100000)
+employee_details('20CS106','Rihana',9876543217,'Pollachi',100000)
